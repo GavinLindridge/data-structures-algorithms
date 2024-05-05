bdea946ef3c61f9d34b2c872d80b830ba505da8e
Gavin Lindridge <gavin.lindridge@jato.com>
2024-05-05 10:59:35 +0100

    feat: implement recursive factorial

---
+++ src/recursion/factorial/factorial.test.ts
@@ -0,0 +1,9 @@
+import { expect, it } from 'vitest'
+import { factorial } from './factorial'
+
+it('count occurrences', () => {
+  expect(factorial(1)).toEqual(1)
+  expect(factorial(2)).toEqual(2)
+  expect(factorial(4)).toEqual(24)
+  expect(factorial(7)).toEqual(5040)
+})
